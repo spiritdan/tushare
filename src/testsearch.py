91ac689351f8b7a18d1061b80fc1692e5b0de834
spiritdan <37323553+spiritdan@users.noreply.github.com>
2019-01-12 07:00:18 +0800

    check panes

--- src/testsearch.py
+++ src/testsearch.py
@@ -7,7 +7,7 @@ from support import log
 from support import databaseConnect
 
 
-
+stock_log = log.logInit("fileoutput") #log初始化
 def update_stock_data(data_list):
     try:
          with databaseConnect.mysql_operator(db='stock') as cursor:
@@ -24,49 +24,52 @@ def update_stock_data(data_list):
 
 def get_details(code,date):
     details=[]
-
-    url = 'http://market.finance.sina.com.cn/transHis.php?symbol={0}&date={1}&page={2}'.format(code, date, 1)
-    req = requests.get(url).content.decode('gbk')
-    #print(req)
-    bscode = bs4.BeautifulSoup(req, 'html.parser')
-    list_info = bscode.select('tbody>tr')
-    page_bs4 = bscode.select('script[language="javascript"]')
-    a = page_bs4[0].getText().split()
-    get_page = [i for i, x in enumerate(a) if x.find('detailPages') != -1]
-    index = int(get_page[0])
-    page_num = eval(a[index].split('=')[-1].replace(';', ''))[-1][0]
-    print(date)
-    print(page_num)
-    for page in range(page_num,0,-1):
-
-        url = 'http://market.finance.sina.com.cn/transHis.php?symbol={0}&date={1}&page={2}'.format(code,date,page)
+    try:
+        url = 'http://market.finance.sina.com.cn/transHis.php?symbol={0}&date={1}&page={2}'.format(code, date, 1)
         req = requests.get(url).content.decode('gbk')
         #print(req)
         bscode = bs4.BeautifulSoup(req, 'html.parser')
         list_info = bscode.select('tbody>tr')
-        page_bs4=bscode.select('script[language="javascript"]')
-        a=page_bs4[0].getText().split()
-        get_page=[i for i, x in enumerate(a) if x.find('detailPages') != -1]
-        index=int(get_page[0])
-        page_num=eval(a[index].split('=')[-1].replace(';',''))[-1][0]
+        page_bs4 = bscode.select('script[language="javascript"]')
+        a = page_bs4[0].getText().split()
+        get_page = [i for i, x in enumerate(a) if x.find('detailPages') != -1]
+        index = int(get_page[0])
+        page_num = eval(a[index].split('=')[-1].replace(';', ''))[-1][0]
         print(date)
-        #print(list_info)
-        #如果列表为空跳出
-        if not list_info:
-            break
-        for info in list_info:
-            temp = []
-            temp.append(date)
-            temp.append(info.select('th')[0].getText())
-            data=info.select('td')
-            for i in range(len(data)):
-                temp.append(data[i].getText())
-            temp.append(info.select('th')[1].getText())
-            details.append(temp)
-            print(temp)
-        time.sleep(5)
-        #print(details)
-    return details
+        print(page_num)
+
+        for page in range(page_num,0,-1):
+
+            url = 'http://market.finance.sina.com.cn/transHis.php?symbol={0}&date={1}&page={2}'.format(code,date,page)
+            req = requests.get(url).content.decode('gbk')
+            #print(req)
+            bscode = bs4.BeautifulSoup(req, 'html.parser')
+            list_info = bscode.select('tbody>tr')
+            page_bs4=bscode.select('script[language="javascript"]')
+            a=page_bs4[0].getText().split()
+            get_page=[i for i, x in enumerate(a) if x.find('detailPages') != -1]
+            index=int(get_page[0])
+            page_num=eval(a[index].split('=')[-1].replace(';',''))[-1][0]
+            print(date)
+            #print(list_info)
+            #如果列表为空跳出
+            if not list_info:
+                break
+            for info in list_info:
+                temp = []
+                temp.append(date)
+                temp.append(info.select('th')[0].getText())
+                data=info.select('td')
+                for i in range(len(data)):
+                    temp.append(data[i].getText())
+                temp.append(info.select('th')[1].getText())
+                details.append(temp)
+                print(temp)
+            time.sleep(5)
+            #print(details)
+        return details
+    except Exception as e:
+        stock_log.error(e)
 
 def string_toDatetime(date):
     day_list=[]
@@ -86,13 +89,12 @@ def string_toDatetime(date):
 
 
 if __name__ == '__main__':
-    details=get_details('sz000002','2004-10-08')
-    print(details)
-'''
+   # details=get_details('sz000002','2004-10-08')
+    #print(details)
 
-    day_list=string_toDatetime('2018-10-08')
+
+    day_list=string_toDatetime('2004-10-08')
     for day in day_list:
-        details = get_details('sz000002', day, 70)
+        details = get_details('sz000002', day)
         update_stock_data(details)
         print(details)
-'''
